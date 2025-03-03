762071a04217e2a6309f9ff2c81e32c45934058d
Caroline GP <caroline-angela@web.de>
2025-03-03 16:06:20 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    @anser = answer
 
     if @question =='I am going to work'
       @answer = 'Great!'
-    elseif @ask.end_with?('?')
+    elsif @question.end_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = 'I do ot care, get dressed and go to work!'
